b889033e69be1559c4530f4a624254f5ac0758cd
Alex <joyking.wang@outlook.com>
2022-03-05 17:34:06 +0800

    init

---
+++ app/src/main/java/com/wangxb/system/app/settings/MainActivity.java
@@ -0,0 +1,64 @@
+package com.wangxb.system.app.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.launcher);
+        Bitmap blur = rsBlur(this, bitmap, 25, 0.25f);
+        ImageView imageView = findViewById(R.id.back);
+        imageView.setBackground(new BitmapDrawable(getResources(), blur));
+    }
+
+    private static Bitmap rsBlur(Context context, Bitmap source, int radius, float scale) {
+        int width = Math.round(source.getWidth() * scale);
+        int height = Math.round(source.getHeight() * scale);
+
+        Bitmap inputBmp = Bitmap.createScaledBitmap(source,width,height,false);
+        //(1)
+        RenderScript renderScript =  RenderScript.create(context);
+
+        Log.i(TAG,"scale size:"+inputBmp.getWidth()+"*"+inputBmp.getHeight());
+
+        // Allocate memory for Renderscript to work with
+        //(2)
+        final Allocation input = Allocation.createFromBitmap(renderScript,inputBmp);
+        final Allocation output = Allocation.createTyped(renderScript,input.getType());
+        //(3)
+        // Load up an instance of the specific script that we want to use.
+        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+        //(4)
+        scriptIntrinsicBlur.setInput(input);
+        //(5)
+        // Set the blur radius
+        scriptIntrinsicBlur.setRadius(radius);
+        //(6)
+        // Start the ScriptIntrinisicBlur
+        scriptIntrinsicBlur.forEach(output);
+        //(7)
+        // Copy the output to the blurred bitmap
+        output.copyTo(inputBmp);
+        //(8)
+        renderScript.destroy();
+
+        return inputBmp;
+    }
+}
